8aa481a1318cc67f18b45d270d8bf011fc119b43
namdecu <namdecu@gmail.com>
2015-11-07 15:41:26 -0800

    commit 2

--- www/js/app.js
+++ www/js/app.js
@@ -5,16 +5,14 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('blogin', ['ionic', 'angularLocalStorage', 'blogin.controllers', 'blogin.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -23,63 +21,61 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.constant('config', {
+  serviceBaseUrl: 'http://192.168.1.5/drupal-7.41/?q=',
+  // serviceBaseUrl: 'http://bobrovdevdev.devcloud.acquia-sites.com/?q=',
+  localStoragePrefix: 'blogin_'
+})
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+.constant('$ionicLoadingConfig', {
+  template: 'Loading...',
+  animation: 'fade-in'
+})
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
+.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $httpProvider) {
+  $httpProvider.defaults.withCredentials = true;
+
+  // Bar with tabs.
+  $stateProvider.state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
+  // Article details page with full post content.
+  .state('tab.category-article', {
+    url: '/category/article/:articleId',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-categories': {
+        templateUrl: 'templates/article-detail.html',
+        controller: 'ArticleDetailCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
+  // All articles tab.
+  .state('tab.articles', {
+    url: '/articles',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-articles': {
+        templateUrl: 'templates/tab-articles.html',
+        controller: 'ArticlesCtrl'
       }
     }
-  });
+  })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  // Article details page with full post content.
+  .state('tab.article-detail', {
+    url: '/articles/:articleId',
+    views: {
+      'tab-articles': {
+        templateUrl: 'templates/article-detail.html',
+        controller: 'ArticleDetailCtrl'
+      }
+    }
+  })
 
-});
+  // default state that shows all articles tab.
+  $urlRouterProvider.otherwise('/tab/articles');
+
+}]);
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,194 @@
-angular.module('starter.services', [])
+angular.module('blogin.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Categories', ['$http', '$q', 'config', function($http, $q, config) {
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+  //Endpoints Variables.
+  var categoriesEndpoint = 'api/categories',
+      categoryEndpoint = 'api/category/';
 
   return {
+
+     /*
+     * Return All Categories.
+     */
     all: function() {
-      return chats;
+      var defer = $q.defer();
+
+      $http({
+        method    : 'GET',
+        url       : config.serviceBaseUrl + categoriesEndpoint,
+        dataType  : 'json',
+      })
+      .success(function(data, status, headers, config) {
+        defer.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        defer.reject(data);
+      });
+
+      return defer.promise;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
+
+    /*
+     * Return Category by ID.
+     */
+    get: function(catId, page) {
+      var defer = $q.defer();
+
+      $http({
+        method    : 'GET',
+        url       : config.serviceBaseUrl + categoryEndpoint + catId,
+        dataType  : 'json',
+        params    : {
+          page: page,
+        },
+      })
+      .success(function(data, status, headers, config) {
+        defer.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        defer.reject(data);
+      });
+
+      return defer.promise;
     }
-  };
-});
+  }
+}])
+
+.factory('Articles', ['$http', '$q', 'config', function($http, $q, config) {
+
+  //Endpoints Variables.
+  var blogEndpoint = 'api/articles',
+      articleEndpoint = 'api/article/';
+
+  return {
+
+     /*
+     * Return All Articles.
+     */
+    all: function(page) {
+      var defer = $q.defer();
+
+      $http({
+        method    : 'GET',
+        url       : config.serviceBaseUrl + blogEndpoint,
+        dataType  : 'json',
+        params    : {
+          page: page,
+        },
+      })
+      .success(function(data, status, headers, config) {
+        defer.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        defer.reject(data);
+      });
+
+      return defer.promise;
+    },
+
+    /*
+     * Return Article by ID.
+     */
+    get: function(articleId) {
+      var defer = $q.defer();
+
+      $http({
+        method    : 'GET',
+        url       : config.serviceBaseUrl + articleEndpoint + articleId,
+        dataType  : 'json',
+      })
+      .success(function(data, status, headers, config) {
+        defer.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        defer.reject(data);
+      });
+
+      return defer.promise;
+    }
+  }
+}])
+
+.factory('Comments', ['$http', '$q', 'config', function($http, $q, config) {
+  //Endpoints Variables.
+  var commentsCheckEndpoint = 'api/comment/countAll',
+      commentsEndpoint = 'api/comments/',
+      postCommentEndpoint = 'api/comment';
+
+  return {
+
+    /*
+     * Check Comments number by Article ID.
+     */
+    check: function(articleId, token) {
+      var defer = $q.defer();
+
+      $http({
+        method    : 'POST',
+        url       : config.serviceBaseUrl + commentsCheckEndpoint,
+        dataType  : 'json',
+        data    : {
+          nid:  articleId,
+        },
+        headers: {
+          'X-CSRF-Token': token,
+        },
+      })
+      .success(function(data, status, headers, config) {
+        defer.resolve(data[0]);
+      })
+      .error(function(data, status, headers, config) {
+        defer.reject(data[0]);
+      });
+
+      return defer.promise;
+    },
+
+    /*
+     * Return Comments by Article ID.
+     */
+    get: function(articleId) {
+      var defer = $q.defer();
+
+      $http({
+        method    : 'GET',
+        url       : config.serviceBaseUrl + commentsEndpoint + articleId,
+        dataType  : 'json',
+      })
+      .success(function(data, status, headers, config) {
+        defer.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        defer.reject(data);
+      });
+
+      return defer.promise;
+    },
+
+     /*
+     * Post Comment to the article.
+     */
+    post: function(commentData, token) {
+      var defer = $q.defer();
+
+      $http({
+        method    : 'POST',
+        url       : config.serviceBaseUrl + postCommentEndpoint,
+        dataType  : 'json',
+        data      : commentData,
+        headers: {
+          'X-CSRF-Token': token,
+        },
+      })
+      .success(function(data, status, headers, config) {
+        defer.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        defer.reject(data);
+      });
+
+      return defer.promise;
+    }
+  }
+}])
